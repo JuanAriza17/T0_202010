3bbac5e7031ffaba1f506eb26df1d2fa471d3562
Juan <ja.arizag@uniandes.edu.co>
2020-01-26 16:43:48 -0500

    Test Arreglo String finalizado, Actualizacion registrada en el repositorio local

--- test/test/logic/TestModelo.java
+++ test/test/logic/TestModelo.java
@@ -31,23 +31,75 @@ public class TestModelo {
 	@Test
 	public void testDarTamano() {
 		// TODO
+		assertEquals(0, modelo.darTamano());
+		
+		setUp2();
+		assertNotNull("El arreglo debería existir",modelo);
+		assertEquals(100, modelo.darTamano());
+		
 	}
 
 	@Test
 	public void testAgregar() {
 		// TODO Completar la prueba
+		assertEquals(0, modelo.darTamano());
+		
+		modelo.agregar("0");
+		assertEquals(1,modelo.darTamano());
+		assertTrue("Debería retornar 0 pero retorna "+modelo.buscar("0")+".",modelo.buscar("0").equals("0"));
+
+		for (int i = 1; i < 200; i++) 
+		{
+			modelo.agregar(""+i);
+			assertEquals(i+1,modelo.darTamano());
+			assertTrue("Debería retornar "+i+ " pero retorna "+modelo.buscar(""+i)+".",modelo.buscar(""+i).equals(""+i));
+		}
+		
 	}
 
 	@Test
 	public void testBuscar() {
 		setUp2();
 		// TODO Completar la prueba
+		assertNotNull("El arreglo debería existir",modelo);
+		assertEquals(100, modelo.darTamano());
+		
+		assertNull("Debería ser null", modelo.buscar("x"));
+		
+		assertTrue("Debería retornar 1 pero retorna "+modelo.buscar("1")+".",modelo.buscar("1").equals("1"));
+
+		
+		for (int i = 0; i < modelo.darTamano(); i++) 
+		{
+			assertTrue("Debería retornar "+i+ " pero retorna "+modelo.buscar(""+i)+".",modelo.buscar(""+i).equals(""+i));
+		}
+
 	}
 
 	@Test
 	public void testEliminar() {
 		setUp2();
 		// TODO Completar la prueba
+		assertNotNull("El arreglo debería existir",modelo);
+		assertEquals(100, modelo.darTamano());
+		
+		String eliminar = modelo.eliminar("x");
+		assertNull("Debería ser null", eliminar);
+		assertEquals(100, modelo.darTamano());
+		
+		String dato = modelo.buscar("1");
+		eliminar = modelo.eliminar("1");
+		assertTrue("Deberían ser el mismo dato",dato.equals(eliminar));
+		assertEquals(99, modelo.darTamano());
+		assertNull("Debeía ser null", modelo.buscar("1"));
+		
+		int numero = 0;
+		for (int i = 0; i < modelo.darTamano(); i++) 
+		{
+			numero++;
+		}
+		assertEquals(modelo.darTamano(), numero);
+
 		
 	}
 
